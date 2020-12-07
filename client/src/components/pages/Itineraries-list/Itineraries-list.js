2b631a67d631640d895e483e4ee6822808d6709f
dreyanfranco <dreyanfranco@gmail.com>
2020-12-07 20:08:51 +0100

    modelo y seed cambiado por spots, rutas de spots

--- client/src/components/pages/Itineraries-list/Itineraries-list.js
+++ client/src/components/pages/Itineraries-list/Itineraries-list.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import ItinerariesService from './../../../service/itineraries.service';
 
 class ItinerariesList extends Component {
     constructor() {
         super()
         this.state = {
-            itineraries: undefined
+            itineraries: []
         }
         this.itinerariesService = new ItinerariesService();
+        // console.log(this.itinerariesService);
     }
 
     componentDidMount = () => {
-        this.itinerariesService
-            .editSpot('5fce436eb003635ae500a61d', '5fce436eb003635ae500a61e')
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-
         // this.itinerariesService
-        //     .getAllItineraries()
-        //     .then(res => this.setState({ itineraries: res.data }))
+        //     .editSpot('5fce436eb003635ae500a61d', '5fce436eb003635ae500a61e')
+        //     .then(res => console.log(res))
         //     .catch(err => console.log(err));
+
+        this.itinerariesService
+            .getAllItineraries()
+            .then(res => this.setState({ itineraries: res.data }))
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
-            <h1>Itineraries list</h1>
-            // this.state.itineraries.map(elm => { elm.name })
+            <>
+                <Container>
+                    <h1>Listado de itinerarios</h1>
+                    <Row>
+                        {this.state.itineraries.map(elm => <Col md={4}>{ elm.name }</Col>)}
+                    </Row>
+                </Container>
+            </>
         )
     }
 }
